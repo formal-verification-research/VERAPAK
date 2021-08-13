fc6e70446da32a1ce6e6bce6ab5ecc49fcaf10f4
Bennett DenBleyer (Laptop) <bennett.denbleyker@usu.edu>
2021-08-13 13:07:01 -0600

    Formatting conventions

--- verification/discrete_search.py
+++ verification/discrete_search.py
@@ -2,18 +2,18 @@ import verification.ve
 
 class DiscreteSearch(ve.VerificationEngine):
 
-    def __init__(self, shouldAttemptChecker, discretePointGenerator, pointChecker):
-        self.shouldAttemptChecker = shouldAttemptChecker
-        self.discretePointGenerator = discretePointGenerator
-        self.pointIsSafe = pointChecker
+    def __init__(self, should_attempt_checker, discrete_point_generator, point_checker):
+        self.should_attempt_checker = should_attempt_checker
+        self.discrete_point_generator = discrete_point_generator
+        self.point_is_safe = point_checker
 
     def verification_impl(self, region):
-        if not self.shouldAttemptChecker():                 # If we shouldn't attempt:
+        if not self.should_attempt_checker():               # If we shouldn't attempt:
             return ve.UNKNOWN, None                         # Return UNKNOWN
         else:                                               # Otherwise
-            points = self.discretePointGenerator(region)    # Get discretized points
+            points = self.discrete_point_generator(region)  # Get discretized points
             for point in points:                            # For each point
-                if not pointIsSafe(point):                  # Check if it's safe
+                if not point_is_safe(point):                # Check if it's safe
                     return ve.UNSAFE, point                 # If not, return UNSAFE
             return ve.SAFE, None                            # Otherwise, return SAFE
 
