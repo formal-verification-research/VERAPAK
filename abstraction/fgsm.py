d1e1cceabb3d7877a448bbb843b95c56eb480eb0
Bennett DenBleyer (Laptop) <bennett.denbleyker@usu.edu>
2021-08-18 21:38:48 -0600

    Abstraction: FGSM (Consistency)

--- abstraction/fgsm.py
+++ abstraction/fgsm.py
@@ -14,11 +14,15 @@ def _min_dim(region):
 
 class FGSMEngine(ae.AbstractionEngine):
 
-    def __init__(self, gradient_function, granularity):
+    def __init__(self, gradient_function, granularity, fallback_strategy=None):
         self.gradient = gradient_function
         self.granularity = granularity
+        self.fallback_strategy = fallback_strategy
 
     def abstraction_impl(self, region, num_abstractions):
+        if self.fallback_strategy is not None and self.fallback_strategy.should_fallback(region, num_abstractions): # Should fallback?
+            return self.fallback_strategy(region, num_abstractions)                                                 # Then fall back
+
         center = CenterPoint().abstraction_impl(region, 1)              # Get center point
         if len(center) == 0:                                            # If no input:
             return []                                                       # Return no abstractions
@@ -28,6 +32,8 @@ class FGSMEngine(ae.AbstractionEngine):
 
             min_dimension, min_dimension_index = _min_dim(region) # TODO: In original, uses std::distance --- Why?
             max_radius = min_dimension.second - min_dimension.first / 1.25 # TODO: Why divide by 1.25
+
+            self._setup(region, num_abstractions, center)
 
             e1_lowerbound = 1
             e1_upperbound = int(max_radius / granularity[min_dimension_index])
@@ -40,27 +46,24 @@ class FGSMEngine(ae.AbstractionEngine):
             
             return retVal
 
-    def _setup(self, region, num_abstractions):
+    def _setup(self, region, num_abstractions, center):
         raise NotImplementedError("FGSMEngine did not implement _setup(self, region, num_abstractions)")
     
-    def _next_point(self, region, ):
-        raise NotImplementedError("FGSMEngine did not implement _next_point(self, region, ...)")
+    def _next_point(self, region, center, gradient_sign, epsilon):
+        raise NotImplementedError("FGSMEngine did not implement _next_point(self, region, center, gradient_sign, epsilon)")
 
 
 class ModFGSM(FGSMEngine):
 
     def __init__(self, gradient_function, granularity, dim_select_strategy, percent_fgsm, fallback_strategy=None):
-        super.__init__(gradient_function, granularity)
+        super.__init__(gradient_function, granularity, fallback_strategy=fallback_strategy)
+
         self.dim_select_strategy = dim_select_strategy
         self.percent_fgsm = percent_fgsm
-        self.fallback_strategy = fallback_strategy
 
-    def _setup(self, region, num_abstractions):
-        if self.fallback_strategy is not None and self.fallback_strategy.should_fallback(region, num_abstractions):
-            return self.fallback_strategy(region, num_abstractions)
-
-        dims = self.dim_select_strategy(region, region.size())
-        num_dims_fgsm = self.percent_fgsm * region.size()
+    def _setup(self, region, num_abstractions, center):
+        dims = self.dim_select_strategy(region, center.size())
+        num_dims_fgsm = self.percent_fgsm * center.size()
 
         self.M = np.zeros(center.size())
         self.M_not = np.one(center.size)
@@ -70,7 +73,7 @@ class ModFGSM(FGSMEngine):
             self.M_not[dims[i]] = 0
 
     def _next_point(self, region, center, gradient_sign, epsilon):
-        R = np.random.Generator.integers(0, 1, region.size(), endpoint=True)
+        R = np.random.Generator.integers(0, 1, center.size(), endpoint=True)
         for i in range(0, center.size()):
             if (R[i] == 0):
                 R[i] = -1
@@ -85,13 +88,10 @@ class ModFGSM(FGSMEngine):
 
 class RplusFGSM(ae.AbstractionEngine):
 
-    def __init__(max_points, gradient, granularity, epsilon):
-        self.max_points = max_points
-        self.gradient = gradient
-        self.granularity = granularity
-        self.epsilon = epsilon
+    def __init__(self, gradient_function, granularity, epsilon, fallback_strategy=None):
+        super.__init__(gradient_function, granularity, fallback_strategy=fallback_strategy)
 
-    def _setup(self, region, num_abstractions):
+    def _setup(self, region, num_abstractions, center):
         pass
 
     def _next_point(self, region, center, gradient_sign, epsilon):
