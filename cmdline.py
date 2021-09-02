4be7be5aae0b311a6480de886cbe9e255a2f63fb
Bennett DenBleyer (Laptop) <bennett.denbleyker@usu.edu>
2021-09-02 17:56:38 -0600

    Command Line Parsing: Add beginnings of a file parser

--- cmdline.py
+++ cmdline.py
@@ -1,19 +1,44 @@
-import argparse
+import argparse # https://docs.python.org/3/library/argparse.htm
 import sys
+import os
 
-parser = argparse.ArgumentParser(description="")
-subparsers = parser.add_subparsers(help="sub-command help")
+parser = argparse.ArgumentParser(prog="VERAPAK", description="")
+parser.add_argument("filepath", type=argparse.FileType('r'), help="Path to the config file") # Places the config file in parsed_args.filepath, already opened and in read mode.
 
-file_parser = subparsers.add_parser("conf", help="Use a config file")
-file_parser.add_argument("filepath", type=argparse.FileType('r'), help="Path to the config file") # Places the config file in parsed_args.filepath, already opened and in read mode.
+def directory(string):
+    if os.path.isdir(string):
+        return string
+    else:
+        raise ValueError(string + " is not a valid directory")
 
-arg_parser = subparsers.add_parser("cmd", help="Supply manually")
-arg_parser.add_argument("initial_point", type=list)
+fileparser = argparse.ArgumentParser()
+fileparser.add_argument("output_directory", type=directory, help="The path where adversarial examples should be sent")
+
+graphgroup = fileparser.add_argument_group("graph")
+graphgroup.add_argument("graph", type=argparse.FileType('r'), help="Path to the graph")
+graphgroup.add_argument("-i", "--input", type=str, help="Graph's input node")
+graphgroup.add_argument("-o", "--output", type=str, help="Graph's output node")
+rad = graphgroup.add_mutually_exclusive_group(required=True)
+rad.add_argument("--radius", nargs=1, help="Single radius to apply to all dimensions")
+rad.add_argument("--radii", nargs="+", help="Per-dimension radii, separated by either commas or spaces") # Pre-parse, split on commas or set nargs to 1
+gran = graphgroup.add_mutually_exclusive_group(required=True)
+gran.add_argument("--granularity", nargs=1, help="Single granularity to apply to all dimensions")
+gran.add_argument("--granularities", nargs="+", help="Per-dimension granularities, separated by commas or spaces") # Pre-parse, split on commas or set nargs to 1
 
 
-def parse_args(args):
+# TODO: Make everything file-based as opposed to command line arguments (except maybe a few smaller, highly-probable-to-change flags like strategies)
+
+def parse_args(args, exit_on_error=True):
     global parsed_args
     parsed_args = parser.parse_args(args)
+    config_lines = parsed_args.filepath.readlines()
 
-if __name__ == "__main__":
-    parse_args(sys.argv[1:])
+
+#    for line in config_lines:
+            
+
+#if __name__ == "__main__":
+#    parse_args(sys.argv[1:])
+
+
+
