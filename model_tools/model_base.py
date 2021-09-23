c29cf87a3f19e54604917ef1997c47bc9d2d9f70
Joshua Smith <joshua.smith@campbellsci.com>
2021-09-23 14:01:58 -0600

    ONNX model loading, evaluating, and gradient of loss wrt input
    * working onnx model load with input and output values and datatypes exposed, working on getting the gradient of the input
    
    * refactor ONNXModel into own file
    
    * use input dtype derived from onnx representation when evaluating model
    
    * calculating gradient of loss (cross entropy) wrt input on onnx models
    
    * remove debug statement

---
+++ model_tools/model_base.py
@@ -0,0 +1,18 @@
+from onnx_tf.backend import prepare
+
+
+class ModelBase:
+    def __init__(self, model, input_shape, output_shape, input_dtype, output_dtype):
+        self.model_internal = model
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+        self.input_dtype = input_dtype
+        self.output_dtype = output_dtype
+
+    def evaluate(self, point):
+        raise NotImplementedError(
+            "Model did not implement the evaluate function")
+
+    def gradient_of_loss_wrt_input(self, point, label):
+        raise NotImplementedError(
+            "Model did not implement the gradient_wrt_input function")
