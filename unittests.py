600e21fd07a2a1082eb631e364c5ec2966ac0252
Joshua Smith <joshua.smith@campbellsci.com>
2021-09-30 14:13:38 -0600

    working gradient base dimension selection

--- unittests.py
+++ unittests.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
-from utilities.point_tools import *
+from utilities import point_tools
+from dimension_ranking import gradient_dim_select
 
 
 class PointToolsTests(unittest.TestCase):
@@ -10,7 +11,7 @@ class PointToolsTests(unittest.TestCase):
         self.region_covering = [np.array([0.5, 3.0, 5.5, -1.5, -2.8, 3.5]),
                                 np.array([1.1, 3.6, 6.1, -0.9, -2.2, 4.1])]
         self.region_outside_covering = [np.array([2.5, 2, 3.5, -6.5, 5.2, -2.5]),
-                               np.array([3.5, 3, 4.5, -5.5, 6.2, -1.5])]
+                                        np.array([3.5, 3, 4.5, -5.5, 6.2, -1.5])]
         self.region_novalid = [np.array([1.1, 3.6, 6.1, -0.9, -2.2, 4.1]),
                                np.array([1.9, 4.4, 6.9, -0.1, -1.2, 4.9])]
         self.region_boundary_both = [np.array([1, 3.5, 6, -1, -2.3, 4]),
@@ -22,36 +23,48 @@ class PointToolsTests(unittest.TestCase):
         self.valid_point2 = np.array([[3, 2.5, 4], [-6, 5.7, -2]])
 
     def test_valid_point_in_region(self):
-        res = get_valid_point_in_region(
+        res = point_tools.get_valid_point_in_region(
             self.region_boundary, self.granularity, self.valid_point)
         self.assertTrue(np.all(self.valid_point == res))
-        res = get_valid_point_in_region(
+        res = point_tools.get_valid_point_in_region(
             self.region_covering, self.granularity, self.valid_point)
         self.assertTrue(np.all(self.valid_point == res))
-        self.assertTrue(is_point_valid(self.valid_point2,
-                                       self.granularity, self.valid_point))
-        res = get_valid_point_in_region(
+        self.assertTrue(point_tools.is_point_valid(self.valid_point2,
+                                                   self.granularity, self.valid_point))
+        res = point_tools.get_valid_point_in_region(
             self.region_outside_covering, self.granularity, self.valid_point)
         self.assertTrue(np.all(self.valid_point2 == res))
-        res = get_valid_point_in_region(
+        res = point_tools.get_valid_point_in_region(
             self.region_novalid, self.granularity, self.valid_point)
         self.assertTrue(res is None)
 
     def test_num_valid_points(self):
-        res = get_amount_valid_points(
+        res = point_tools.get_amount_valid_points(
             self.region_boundary, self.granularity, self.valid_point)
         self.assertEqual(res, 1)
-        res = get_amount_valid_points(
+        res = point_tools.get_amount_valid_points(
             self.region_novalid, self.granularity, self.valid_point)
         self.assertEqual(res, 0)
-        res = get_amount_valid_points(
+        res = point_tools.get_amount_valid_points(
             self.region_boundary_both, self.granularity, self.valid_point)
         self.assertEqual(res, 1)
-        res = get_amount_valid_points(
+        res = point_tools.get_amount_valid_points(
             self.region_multiple, self.granularity, self.valid_point)
         self.assertEqual(res, 5**6)
 
     def tearDown(self):
+        pass
+
+
+class GradientDimSelectionTests(unittest.TestCase):
+    def setUp(self):
+        self.grad_strategy = gradient_dim_select.GradientBasedDimSelection(
+            lambda x: x)
+
+    def test_dim_selection_strategy(self):
+        region_lb = np.array([1,2,3,4,5,6])
+        a = self.grad_strategy.rank_indices([region_lb, region_lb + 4])
+        print(a)
         pass
 
 
