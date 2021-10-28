a3e9ac8465d19e548283876fdceb00643270509e
Bennett DenBleyker <bennett.denbleyker@usu.edu>
2021-10-28 14:59:35 -0600

    Change configuration

--- vnncomptests/makeconf.py
+++ vnncomptests/makeconf.py
@@ -1,6 +1,10 @@
 import sys
 
+if len(sys.argv) < 4:
+    sys.exit(1)
+
 graph = sys.argv[1]
 vnnlib = sys.argv[2]
+epsilon = sys.argv[3]
 
-print("graph :: " + graph + "\nvnn :: " + vnnlib + "\nabstraction_strategy :: rfgsm\nverification_strategy :: discrete_search\ngranularity :: 0.00390625\ndomain_lower_bound :: -100.0\ndomain_upper_bond :: 100.0")
+print(f"graph :: {graph}\nvnnlib :: {vnnlib}\nabstraction_strategy :: rfgsm\nverification_strategy :: discrete_search\ngranularity :: {epsilon}\ndomain_lower_bound :: -100.0\ndomain_upper_bound :: 100.0")
