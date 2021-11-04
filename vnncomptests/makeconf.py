d9107e8ad0f17ec06569f32dd5839b74059bead4
Bennett DenBleyker <bennett.denbleyker@usu.edu>
2021-11-04 16:56:04 -0600

    Configs

--- vnncomptests/makeconf.py
+++ vnncomptests/makeconf.py
@@ -7,4 +7,4 @@ graph = sys.argv[1]
 vnnlib = sys.argv[2]
 epsilon = sys.argv[3]
 
-print(f"graph :: {graph}\nvnnlib :: {vnnlib}\nabstraction_strategy :: rfgsm\nverification_strategy :: discrete_search\ngranularity :: {epsilon}\ndomain_lower_bound :: -100.0\ndomain_upper_bound :: 100.0")
+print(f"graph :: {graph}\nvnnlib :: {vnnlib}\nabstraction_strategy :: rfgsm\nverification_strategy :: discrete_search\ngranularity :: {epsilon}")
