7e247278e2a9d7ed1b7cbdb10bb37ed6397292f5
Bennett DenBleyker <bennett.denbleyker@usu.edu>
2021-11-10 13:00:58 -0700

    Change run_instance to use seconds

--- vnncompscripts/run_instance.sh
+++ vnncompscripts/run_instance.sh
@@ -3,11 +3,11 @@ CATEGORY=$2
 ONNX=$3
 VNNLIB=$4
 RESULTS_FILE=$5
-TIMEOUT=$(echo "$6/60" | bc)
+TIMEOUT=$6
 
 echo $TIMEOUT
 
-docker run --rm -it -v $PWD/vnncomptests:/src/vnncomptests -v $PWD/out:/src/out verapak:latest python main.py --config_file=vnncomptests/vnncomp.conf --output_dir=out --timeout_minutes=$TIMEOUT
+docker run --rm -it -v $PWD/vnncomptests:/src/vnncomptests -v $PWD/out:/src/out verapak:latest python main.py --config_file=vnncomptests/vnncomp.conf --output_dir=out --timeout=$TIMEOUT
 
 mkdir out/$CATEGORY 2> /dev/null
 mv out/adversarial_example.npy	out/$CATEGORY/$ONNX-$VNNLIB.npy
