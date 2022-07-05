923539a7c5ffbccd0df2b08afd91fd363945b25d
Bennett DenBleyker <bennett.denbleyker@usu.edu>
2022-07-05 13:40:47 -0600

    auto-install Docker

--- vnncompscripts/install_tool.sh
+++ vnncompscripts/install_tool.sh
@@ -1,5 +1,9 @@
 docker -v
 if [ $? == 127 ]; then
-	apt-get -y install docker
+	apt install apt-transport-https ca-certificates curl software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+	apt-cache policy docker-ce
+	apt install docker-ce
 fi
 docker build -t verapak:latest ..
