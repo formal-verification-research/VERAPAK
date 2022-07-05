2d73b6f4aac613c046aa01a7ad84c5ef059b44f0
Bennett DenBleyker <bennett.denbleyker@usu.edu>
2022-07-05 13:49:25 -0600

    Add -y flag

--- vnncompscripts/install_tool.sh
+++ vnncompscripts/install_tool.sh
@@ -1,9 +1,9 @@
 docker -v
 if [ $? == 127 ]; then
-	apt install apt-transport-https ca-certificates curl software-properties-common
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+	sudo apt install apt-transport-https ca-certificates curl software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 	apt-cache policy docker-ce
-	apt install docker-ce
+	sudo apt -y install docker-ce
 fi
 docker build -t verapak:latest ..
